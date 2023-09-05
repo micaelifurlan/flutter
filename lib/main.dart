b1fe2d6ab1a6e55148217d23f34d4838de0bb2b0
micaelifurlan <micaelisf@gmail.com>
2023-09-04 21:29:06 -0300

    avaliacao

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,25 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
   State<MyApp> createState() => _MyAppState();
+  Widget meuLayoutWidget() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Text(
+        "Valor",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
 }
 
 class _MyAppState extends State<MyApp> {
   containerBuild(
-      {Color bgColor = Colors.yellowAccent,
-      double w = 50,
+      {Color bgColor = Colors.green,
+      double w = 120,
       double h = 50,
-      double radius = 0}) {
+      double radius = 5}) {
     return Container(
-      margin: const EdgeInsets.all(5),
+      margin: const EdgeInsets.all(40),
       width: w,
       height: h,
       decoration: BoxDecoration(
@@ -26,22 +35,27 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
+  Widget meuLayoutWidget() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Text(
+        "Valor",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(title: const Text("App Bar")),
+          appBar: AppBar(title: const Text("Avaliação 1")),
           body: Center(
             child: Container(
-              color: Colors.black,
-              width: 250,
-              height: 250,
+              width: 400,
+              height: 400,
               child: Wrap(
                 alignment: WrapAlignment.center,
                 children: [
-                  containerBuild(),
-                  containerBuild(),
-                  containerBuild(),
                   containerBuild(),
                   containerBuild(),
                 ],
